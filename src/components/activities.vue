fd496cba90793cf4c4c098bf9a700b021d0bbbe3
Tom <2119056263@qq.com>
2023-03-17 21:52:42 +0800

    更改

--- src/components/activities.vue
+++ src/components/activities.vue
@@ -1,22 +1,28 @@
 <template>
     <div class="tab">
         <el-card class="box-card">
-      <template #header>
-        <div class="card-header">
-          <span>活动</span>
-        </div>
-      </template>
-      <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+          <div>
+            <span style="font-family: yuanshen">「风物之歌」交流平台限时开启，一起来交流和分享美妙音乐吧</span>
+            <br/><br/>
+            <span style="font-family: yuanshen">【有奖活动】「风花的呼吸」3.5版本攻略征集活动开启！</span>
+            <br/><br/>
+            <span style="font-family: yuanshen">【有奖活动】3.5版本第一期话题活动开启！发帖赢原石~</span>
+            <br/>
+          </div>
     </el-card>
     </div>
 </template>
   
 
 <style>
+span {
+  font-size: 20px;
+}
 .card-header {
 display: flex;
 justify-content: space-between;
 align-items: center;
+height: 30px;
 }
 
 .text {
@@ -28,6 +34,7 @@ margin-bottom: 18px;
 }
 
 .box-card {
-width: 480px;
+width: 450px;
+height: 240px;
 }
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,12 +9,20 @@
       <div class="carousel">
         <el-carousel indicator-position="outside">
           <el-carousel-item v-for="item in 4" :key="item">
-            <h3 text="2xl" justify="center">{{ item }}</h3>
+           
           </el-carousel-item>
         </el-carousel>
       </div>
       <div class="demo">
         <div class="title">
+          <button
+            v-for="tab in tabs"
+            :key="tab"
+            :class="['tab-button', { active: currentTab === tab }]"
+            @click="currentTab = tab"
+          >
+            {{ tab }}
+          </button>
           <ul>  
             <span style="font-family: yuanshen">活动</span>
             <span style="font-family: yuanshen">公告</span>
@@ -22,7 +30,7 @@
           </ul>
         </div>
         <div class="text">
-          
+          <component :is="currentTab" class="tab"></component>
         </div>
       </div>
     </div>
@@ -43,7 +51,7 @@ export default {
   data() {
     return {
       currentTab: 'Home',
-      tabs: ['活动', '公告', '资讯']
+      tabs: ['activities', 'announcement', 'info']
     }
   }
 }
